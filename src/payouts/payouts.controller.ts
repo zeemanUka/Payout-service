bfb88da503bb3585f99388403e2e9c03b3e69e93
Tobechukwu Uka <ukadavid7@gmail.com>
2026-01-21 11:29:28 +0100

    feat: add payout endpoints and request validation

--- src/payouts/payouts.controller.ts
+++ src/payouts/payouts.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { PayoutsService } from './payouts.service';
+import { PayoutRequestDto } from './dto/payout-request.dto';
 
 @Controller('payouts')
-export class PayoutsController {}
+export class PayoutsController {
+  constructor(private payouts: PayoutsService) {}
+
+  @Post('process')
+  async process(@Body() dto: PayoutRequestDto) {
+    return this.payouts.processPayout({
+      merchantId: dto.merchantId,
+      amount: dto.amount,
+      currency: dto.currency,
+      idempotencyKey: dto.idempotencyKey
+    });
+  }
+
+  @Get(':id')
+  async get(@Param('id') payoutId: string) {
+    return this.payouts.getPayout(payoutId);
+  }
+}
